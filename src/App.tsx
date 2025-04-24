3da4eec867e0f4d19aabb883c6ae45b572cf8e46
mazenhadjali <hadjalimazen@gmail.com>
2025-04-24 13:29:02 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container text-center mx-auto mt-10">
+        <h1 className="text-3xl font-bold my-4">
+          Proxym Group
+        </h1>
+        <p className="text-lg">This is a simple React app with Tailwind CSS.</p>
+        <p className="text-lg p-4">
+          Form Fate Builder
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
