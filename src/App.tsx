01dcd643394233d96ac6977d03ac51111008a024
mazenhadjali <hadjalimazen@gmail.com>
2025-04-28 11:35:17 +0100

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-
 function App() {
 
   return (
     <>
-      <div className="container text-center mx-auto mt-10">
-        <h1 className="text-3xl font-bold my-4">
-          Proxym Group
-        </h1>
-        <p className="text-lg">This is a simple React app with Tailwind CSS.</p>
-        <p className="text-lg p-4">
+      <div className="text-center mx-auto mt-5">
+        <div>
+          {/* image /Logo-Proxym-2020-02.png */}
+          <img src="/Logo-Proxym-2020-02.png" alt="Proxym" className="mx-auto mb-2 max-w-[200px]" />
+        </div>
+        <p className="font-bold text-xl p-1 text-orange-500">
           Form Fate Builder
         </p>
+        <div className="container mx-auto border border-amber-600 rounded-lg shadow-lg p-4 mt-2 h-full bg-gradient-to-r from-green-100 to-pink-50">
+          <p className="text-gray-800 text-lg">
+            This is a simple form fate builder for Proxym.
+          </p>
+          <p className="text-gray-800 text-lg">
+            You can create your own form and use it in your application.
+          </p>
+        </div>
+        <div className="px-3">
+          <div className="w-full border border-amber-600 rounded-lg shadow-lg p-4 mt-2 h-full bg-gradient-to-r from-blue-100 to-orange-200-50 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {/* first col */}
+            <div className="col-span-1">
+              <p className="text-gray-800 text-lg">
+                This here is the configuration of the form.
+              </p>
+              <p className="text-gray-800 text-lg">
+                You can add, edit and delete fields.
+              </p>
+            </div>
+            {/* second col  merge 2 cols in case of lg */}
+            <div className="col-span-1 lg:col-span-2">
+              <p className="text-gray-800 text-lg">
+                This is the form preview.
+              </p>
+              <p className="text-gray-800 text-lg">
+                You can see how the form will look like.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
